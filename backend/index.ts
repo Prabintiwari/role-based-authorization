ceeb579efca31b2b9c4e38248d7581c556fb27de
unknown <prabintiwari964@gmail.com>
2025-12-10 13:25:07 +0100

    adding file handling,email handling

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({origin:"http://localhost:5173"}));
+app.use(cors({origin:process.env.FRONTEND_URL}));
 app.use(express.json());
 
 // Routes
