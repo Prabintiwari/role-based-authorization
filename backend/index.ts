fa5e042aab84dfa1bb262c6257cf8be49b66f7d5
unknown <prabintiwari964@gmail.com>
2025-12-10 14:28:06 +0100

    connecting backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRoutes from "./routes/userRoutes"
-import productRoutes from "./routes/productRoutes"
-import cartRoutes from "./routes/cartRoutes"
-import fileUploadRoute from "./routes/fileUploadsRoutes"
-import  responseHandler  from './middleware/responseHandler';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import cartRoutes from "./routes/cartRoutes";
+import fileUploadRoute from "./routes/fileUploadsRoutes";
+import responseHandler from "./middleware/responseHandler";
 
 dotenv.config();
 
@@ -13,23 +13,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({origin:process.env.FRONTEND_URL}));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/carts',cartRoutes)
-app.use('/api/fileupload',fileUploadRoute)
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/fileupload", fileUploadRoute);
 app.use("/api/uploads", express.static("uploads"));
 
-app.use(responseHandler)
+app.use(responseHandler);
 
-
-app.get('/', (req, res) => {
-  res.json({ status: 'OK', message: 'Server is running' });
+app.get("/", (req, res) => {
+  res.json({ status: "OK", message: "Server is running" });
 });
-
 
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
@@ -37,4 +35,4 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
-export default app
+export default app;
